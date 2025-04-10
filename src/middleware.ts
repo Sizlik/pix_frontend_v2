15bcdc70f48a76e88fde3a911219974153a7bfdf
sizlik <zenjagems@gmail.com>
2025-04-10 12:41:12 +0300

    temp

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,23 +3,23 @@ import { cookies } from "next/headers";
 import { NextResponse, NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  // const { searchParams } = new URL(request.url);
-  // console.log(searchParams.get("_rsc"));
-  // // if (search.get("_rsc")) {
-  // //   return NextResponse.next();
-  // // }
-  //
-  // const token = cookies().get("token")?.value;
-  // if (token) {
-  //   const userData = await CheckToken({ token: token });
-  //   const response = NextResponse.next();
-  //   response.cookies.set("user", JSON.stringify(userData));
-  //   if (userData) return response;
+  const { searchParams } = new URL(request.url);
+  console.log(searchParams.get("_rsc"));
+  // if (search.get("_rsc")) {
+  //   return NextResponse.next();
   // }
-  //
-  // return NextResponse.redirect(new URL("/", request.url));
+  
+  const token = cookies().get("token")?.value;
+  if (token) {
+    const userData = await CheckToken({ token: token });
+    const response = NextResponse.next();
+    response.cookies.set("user", JSON.stringify(userData));
+    if (userData) return response;
+  }
+  
+  return NextResponse.redirect(new URL("/", request.url));
 }
 
-// export const config = {
-//   matcher: "/dashboard/:path*",
-// };
+export const config = {
+  matcher: "/dashboard/",
+};
