d4f79f4691825eeabd168658f9f66f1f97e6efa5
eugene <zenjagems@gmail.com>
2025-04-10 20:34:46 +0200

    a

--- src/middleware.ts
+++ src/middleware.ts
@@ -21,7 +21,6 @@ export async function middleware(request: NextRequest) {
 export const config = {
   matcher: "/dashboard/:path*",
   missing: [
-    { type: 'header', key: 'next-router-prefetch' },
-    { type: 'header', key: 'purpose', value: 'prefetch' },
+    { type: 'header', key: 'next-router-state-tree' },
   ],
 };
